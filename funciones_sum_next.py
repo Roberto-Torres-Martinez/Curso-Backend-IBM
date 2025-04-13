6555511ca46cffd7ec45ec05e07b39c4b740ed95
RobertoTorres <roberxiri@gmail.com>
2025-04-13 19:35:16 +0200

    Segundo Ejercicio Funciones Sum_Next

---
+++ funciones_sum_next.py
@@ -0,0 +1,21 @@
+#Función sum
+#Suma de todos los elementos
+lista = [1, 2, 3, 4, 5]
+resultado = sum(lista)
+print(f'Resultado de la suma: {resultado}')
+
+#Proporcionar un valor inicial
+resultado = sum(lista, 20)
+print(f'Resultado de la suma con valor inicial: {resultado}')
+
+#Función next
+iterador = iter(lista)
+
+#Obtenemos el próximo elemento del iterador
+#elemento = next(iterador)
+print(f'Siguiente elemento del iterable: {next(iterador)}')
+print(f'Siguiente elemento del iterable: {next(iterador)}')
+print(f'Siguiente elemento del iterable: {next(iterador)}')
+print(f'Siguiente elemento del iterable: {next(iterador)}')
+print(f'Siguiente elemento del iterable: {next(iterador)}')
+print(f'Siguiente elemento del iterable: {next(iterador)}')
