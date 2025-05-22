afdf9452daaecedeba410c50297f32dab2f89ce3
RobertoTorres <roberxiri@gmail.com>
2025-05-22 18:38:22 +0200

    App messenger añadida y tests para la app messenger

--- Django/web_playground/webplayground_project/webplayground/settings.py
+++ Django/web_playground/webplayground_project/webplayground/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'messenger',
     'pages.apps.PagesConfig',
     'ckeditor',
     'profiles',
