b00e69001f827e9d7d1477bb1f4266f087379158
RobertoTorres <roberxiri@gmail.com>
2025-05-24 12:16:57 +0200

    App webplaygound terminada

--- Django/web_playground/webplayground_project/messenger/models.py
+++ Django/web_playground/webplayground_project/messenger/models.py
@@ -28,7 +28,12 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
+    alerta = models.BooleanField(default=False)
     objects = ThreadManager()
+    
+    class Meta:
+        ordering = ['-updated']
     
 def messages_changed(sender, **kwargs):
     instance = kwargs.pop('instance', None)
@@ -45,5 +50,7 @@ def messages_changed(sender, **kwargs):
                 false_pk_set.add(msg_pk)
                 
     pk_set.difference_update(false_pk_set)
+    
+    instance.save()
 
 m2m_changed.connect(messages_changed, sender=Thread.messages.through)
